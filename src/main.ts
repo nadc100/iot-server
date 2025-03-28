949d353b6f17444035cd14fa3e5dec24b3df7786
ingenieriafisica <99398123+ingenieriafisica@users.noreply.github.com>
2025-03-28 06:09:46 -0500

    cross

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  app.enableCors()
   const app = await NestFactory.create(AppModule);
+  app.useLogger(app.get(Logger));
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders:'Content-Type,Autorization'
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
